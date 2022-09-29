7c8db3d49cba93af629f00b33934af33b994b1e5
Jader Neira <jader.neira@gmail.com>
2022-09-29 18:20:24 -0500

    change to spanish

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -3,8 +3,11 @@ import addHours from 'date-fns/addHours';
 
 import Modal from 'react-modal';
 
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import DatePicker, { registerLocale } from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+import es from 'date-fns/locale/es';
+registerLocale('es', es)
 
 const customStyles = {
     content: {
@@ -69,6 +72,9 @@ export const CalendarModal = () => {
                         onChange={ (event) => onDateChanged(event, 'start') }
                         className='form-control'
                         dateFormat='Pp'
+                        showTimeSelect
+                        locale='es'
+                        timeCaption='Hora'
                     />
                 </div>
 
@@ -80,6 +86,9 @@ export const CalendarModal = () => {
                         onChange={ (event) => onDateChanged(event, 'end') }
                         className='form-control'
                         dateFormat='Pp'
+                        showTimeSelect
+                        locale='es'
+                        timeCaption='Hora'
                     />
                 </div>
 
