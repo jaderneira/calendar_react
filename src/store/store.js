f823c2cd4870cf45bb7d564ecf5f49c64a6576c8
Jader Neira <jader.neira@gmail.com>
2022-09-30 16:41:07 -0500

    config useState with the forms

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { calendarSlice, uiSlice } from './';
+import { calendarSlice, uiSlice, authSlice } from './';
 
 
 export const store = configureStore({
     reducer:{
-        calendar: calendarSlice.reducer,
-        ui: uiSlice.reducer,
+        auth:       authSlice.reducer,
+        calendar:   calendarSlice.reducer,
+        ui:         uiSlice.reducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false
